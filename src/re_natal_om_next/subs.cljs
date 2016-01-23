04d91e1e78f609db47038c4b6a1f4324b78f9ff3
Sean Tempesta <sean.tempesta@gmail.com>
2016-01-23 14:41:24 -0500

    Delete the db.cljs, handlers.cljs, and subs.cljs files in src/<project_name>

--- src/re_natal_om_next/subs.cljs
+++
@@ -1,9 +0,0 @@
-(ns re-natal-om-next.subs
-  (:require-macros [reagent.ratom :refer [reaction]])
-  (:require [re-frame.core :refer [register-sub]]))
-
-(register-sub
-  :get-greeting
-  (fn [db _]
-    (reaction
-      (get @db :greeting))))
